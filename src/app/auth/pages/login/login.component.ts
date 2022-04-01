0113858232a7f8534ecc09181ede4fef34d13b09
Roger-Rys <roger.reyesm97@gmail.com>
2022-03-31 22:00:36 -0500

    Uso de router.navigate y plantilla login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    //Ir al backend
+    // verificar user
+    this.router.navigate(["\heroes"])
+
   }
 
 }
